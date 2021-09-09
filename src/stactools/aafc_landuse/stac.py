be56ba5423d57f76b96c68ec46ddf545038f55d2
bluegeo <devin.cairns@bluegeo.ca>
2021-09-09 10:18:23 -0700

    Refactor asset structure * metadata/cog optional * use stactools.core cogify * Add label/file extensions * Add summary and assets * Reconfigure tests

--- src/stactools/aafc_landuse/stac.py
+++ src/stactools/aafc_landuse/stac.py
@@ -1,26 +1,42 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from typing import Optional, List, Any
 import re
+import fsspec
 import pytz
 import logging
 from stactools.aafc_landuse.constants import (
+    JSONLD_HREF,
     LANDUSE_ID,
+    LANDUSE_EPSG,
     LANDUSE_TITLE,
     DESCRIPTION,
     LANDUSE_PROVIDER,
     LICENSE,
     LICENSE_LINK,
+    CLASSIFICATION_VALUES,
 )
 
 import pystac
+from pystac.extensions.item_assets import AssetDefinition, ItemAssetsExtension
+from pystac.extensions.file import FileExtension
+from pystac.extensions.projection import ProjectionExtension
+from pystac.extensions.raster import DataType, RasterBand, RasterExtension, Sampling
+from pystac.extensions.label import (
+    LabelClasses,
+    LabelExtension,
+    LabelTask,
+    LabelType,
+)
 import rasterio
-from rasterio.warp import transform_bounds
 from shapely import geometry
 
 logger = logging.getLogger(__name__)
 
 
-def create_collection(metadata: dict, metadata_url: str) -> pystac.Collection:
+def create_collection(
+        metadata: dict,
+        metadata_url: Optional[str] = JSONLD_HREF) -> pystac.Collection:
     """Create a STAC Collection using a jsonld file provided by AAFC
     and save it to a destination.
 
@@ -28,7 +44,7 @@ def create_collection(metadata: dict, metadata_url: str) -> pystac.Collection:
 
     Args:
         metadata (dict): metadata parsed from jsonld
-        metadata_url (str): Location to save the output STAC Collection json
+        metadata_url (str, optional): Location of metadata
 
     Returns:
         pystac.Collection: pystac collection object
@@ -58,21 +74,93 @@ def create_collection(metadata: dict, metadata_url: str) -> pystac.Collection:
     )
     collection.add_link(LICENSE_LINK)
 
-    collection.set_self_href(metadata_url)
+    # Add asset
+    collection.add_asset(
+        "metadata",
+        pystac.Asset(
+            href=metadata_url,
+            media_type=pystac.MediaType.JSON,
+            roles=["metadata"],
+            title="AAFC Land Use collection metadata",
+        ),
+    )
 
-    collection.save_object()
+    collection_label = LabelExtension.summaries(collection,
+                                                add_if_missing=True)
+    collection_label.label_type = [LabelType.RASTER]
+    collection_label.label_tasks = [LabelTask.CLASSIFICATION]
+    collection_label.label_properties = None
+    collection_label.label_classes = [
+        # TODO: The STAC Label extension JSON Schema is incorrect.
+        # https://github.com/stac-extensions/label/pull/8
+        # https://github.com/stac-utils/pystac/issues/611
+        # When it is fixed, this should be None, not the empty string.
+        LabelClasses.create(list(CLASSIFICATION_VALUES.values()), "")
+    ]
+
+    collection_proj = ProjectionExtension.summaries(collection,
+                                                    add_if_missing=True)
+    collection_proj.epsg = [LANDUSE_EPSG]
+
+    collection_item_asset = ItemAssetsExtension.ext(collection,
+                                                    add_if_missing=True)
+    collection_item_asset.item_assets = {
+        "metadata":
+        AssetDefinition(
+            dict(
+                type=pystac.MediaType.JSON,
+                roles=["metadata"],
+                title="AAFC Land Use metadata",
+            )),
+        "landuse":
+        AssetDefinition({
+            "type":
+            pystac.MediaType.COG,
+            "roles": [
+                "data",
+                "labels",
+                "labels-raster",
+            ],
+            "title":
+            "AAFC Land Use COG",
+            "raster:bands": [
+                RasterBand.create(
+                    nodata=127,
+                    sampling=Sampling.AREA,
+                    data_type=DataType.UINT8,
+                    spatial_resolution=30,
+                ).to_dict()
+            ],
+            "file:values": [{
+                "values": [value],
+                "summary": summary
+            } for value, summary in CLASSIFICATION_VALUES.items()],
+            "label:type":
+            collection_label.label_type[0],
+            "label:tasks":
+            collection_label.label_tasks,
+            "label:properties":
+            None,
+            "label:classes": [collection_label.label_classes[0].to_dict()],
+            "proj:epsg":
+            collection_proj.epsg[0],
+        }),
+    }
 
     return collection
 
 
-def create_item(metadata: dict, metadata_url: str,
-                cog_href: str) -> pystac.Item:
+def create_item(
+    metadata: dict,
+    metadata_url: Optional[str] = JSONLD_HREF,
+    cog_href: Optional[str] = None,
+) -> pystac.Item:
     """Creates a STAC item for a 1990, 2000 and 2010 Canada Land Use dataset.
 
     Args:
         metadata (dict): Contents of the AAFC Land Use jsonld metadata
-        metadata_url (str): Output path for the STAC json
-        cog_href (str): Location of associated COG asset
+        metadata_url (str, optional): Output path for the STAC metadata json
+        cog_href (str, optional): Location of associated COG asset
 
     Returns:
         pystac.Item: STAC Item object.
@@ -93,22 +181,15 @@ def create_item(metadata: dict, metadata_url: str,
     start_datetime = dataset_datetime
     end_datetime = end_datetime
 
-    src = rasterio.open(cog_href)
-
-    bounds = src.bounds
-    bbox = list(transform_bounds(src.crs, "EPSG:4326", *bounds))
+    bbox = list(
+        geometry.Polygon(metadata["geom_metadata"]["coordinates"][0]).bounds)
 
     polygon = geometry.box(*bbox, ccw=True)
     coordinates = [list(i) for i in list(polygon.exterior.coords)]
 
     extent_geometry = {"type": "Polygon", "coordinates": [coordinates]}
 
-    properties = {
-        "title": title,
-        "description": description,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-    }
+    properties = {"title": title, "description": description}
 
     # Create item
     item = pystac.Item(
@@ -124,29 +205,90 @@ def create_item(metadata: dict, metadata_url: str,
         item.common_metadata.start_datetime = start_datetime
         item.common_metadata.end_datetime = end_datetime
 
+    # Add projection extension
+    item_projection = ProjectionExtension.ext(item, add_if_missing=True)
+    item_projection.epsg = LANDUSE_EPSG
+
+    if cog_href is not None:
+        with rasterio.open(cog_href) as dataset:
+            item_projection.bbox = list(dataset.bounds)
+            item_projection.transform = list(dataset.transform)
+            item_projection.shape = [dataset.height, dataset.width]
+
+    # Add label extension
+    item_label = LabelExtension.ext(item, add_if_missing=True)
+    item_label.label_type = LabelType.RASTER
+    item_label.label_tasks = [LabelTask.CLASSIFICATION]
+    item_label.label_properties = None
+    item_label.label_description = ""
+    item_label.label_classes = [
+        # TODO: The STAC Label extension JSON Schema is incorrect.
+        # https://github.com/stac-extensions/label/pull/8
+        # https://github.com/stac-utils/pystac/issues/611
+        # When it is fixed, this should be None, not the empty string.
+        LabelClasses.create(list(CLASSIFICATION_VALUES.values()), "")
+    ]
+
     # Create metadata asset
     item.add_asset(
-        "json",
+        "metadata",
         pystac.Asset(
-            href=cog_href,
+            href=metadata_url,
             media_type=pystac.MediaType.JSON,
             roles=["metadata"],
-            title="JSON metadata",
+            title="AAFC Land Use item metadata",
         ),
     )
-
-    item.add_asset(
-        "cog",
-        pystac.Asset(
+    if cog_href is not None:
+        # Create COG asset if it is provided
+        cog_asset = pystac.Asset(
             href=cog_href,
             media_type=pystac.MediaType.COG,
-            roles=["data"],
-            title=title,
-        ),
-    )
+            roles=["data", "labels", "labels-raster"],
+            title="AAFC Land Use item COG",
+        )
+        item.add_asset("landuse", cog_asset)
 
-    item.set_self_href(metadata_url)
+        # File Extension
+        cog_asset_file = FileExtension.ext(cog_asset, add_if_missing=True)
+        mapping: List[Any] = [{
+            "values": [value],
+            "summary": summary
+        } for value, summary in CLASSIFICATION_VALUES.items()]
+        cog_asset_file.values = mapping
+        with fsspec.open(cog_href) as file:
+            size = file.size
+            if size is not None:
+                cog_asset_file.size = size
 
-    item.save_object()
+        # Raster Extension
+        cog_asset_raster = RasterExtension.ext(cog_asset, add_if_missing=True)
+        cog_asset_raster.bands = [
+            RasterBand.create(
+                nodata=0,
+                sampling=Sampling.AREA,
+                data_type=DataType.UINT8,
+                spatial_resolution=30,
+            )
+        ]
+
+        # Complete the projection extension
+        cog_asset_projection = ProjectionExtension.ext(cog_asset,
+                                                       add_if_missing=True)
+        cog_asset_projection.epsg = item_projection.epsg
+        cog_asset_projection.bbox = item_projection.bbox
+        cog_asset_projection.transform = item_projection.transform
+        cog_asset_projection.shape = item_projection.shape
+
+        # Complete the label Extension
+        cog_asset.extra_fields["label:type"] = item_label.label_type
+        cog_asset.extra_fields["label:tasks"] = item_label.label_tasks
+        cog_asset.extra_fields[
+            "label:properties"] = item_label.label_properties
+        cog_asset.extra_fields[
+            "label:description"] = item_label.label_description
+        cog_asset.extra_fields["label:classes"] = [
+            item_label.label_classes[0].to_dict()
+        ]
 
     return item
